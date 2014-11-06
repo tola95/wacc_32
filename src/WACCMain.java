8abf1ed8cbb6f632c3f490727fd709ff863a96f0
Sumnima Subba <Sumi@dyn1188-24.wlan.ic.ac.uk>
2014-11-06 10:25:09 +0000

    pulling

--- src/WACCMain.java
+++ src/WACCMain.java
@@ -1,8 +1,15 @@
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
 public class WACCMain {
 	public static void main(String[] args) throws Exception {
+		String inputFile = null;
+		if (args.length > 0) inputFile = args[0];
+		InputStream is = System.in;
+		if (inputFile != null) is = new FileInputStream(inputFile);
 		ANTLRInputStream input = new ANTLRInputStream(System.in);
 		BasicLexer lexer = new BasicLexer(input);
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
